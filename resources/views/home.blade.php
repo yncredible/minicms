89c9c3424f107fb10ed98c120168946c3c39fa96
Yannick Nijs <info.yannicknijs@gmail.com>
2015-12-22 21:38:50 +0100

    basic app templating, created view for the homepage

---
+++ resources/views/home.blade.php
@@ -0,0 +1,5 @@
+@extends('templates.app')
+@section('content')
+	<h1>Welkom</h1>
+@stop
+
---
+++ resources/views/templates/partials/navigation.blade.php
@@ -0,0 +1,27 @@
+<nav class="navbar navbar-default" role="navigation">
+	<!-- Brand and toggle get grouped for better mobile display -->
+	<div class="container">
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-ex1-collapse">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a class="navbar-brand" href="/">SCRAP3R</a>
+		</div>
+
+		<!-- Collect the nav links, forms, and other content for toggling -->
+		<div class="collapse navbar-collapse navbar-ex1-collapse">
+			<ul class="nav navbar-nav">
+				<li><a href="/">Home</a></li>
+			</ul>
+			<ul class="nav navbar-nav navbar-right">
+				<li><a href="#">Add</a></li>
+				<li><a href="#">Log out</a></li>
+				<li><a href="#">Sign up</a></li>
+				<li><a href="#">Log in</a></li>
+			</ul>
+		</div><!-- /.navbar-collapse -->
+	</div>
+</nav>
