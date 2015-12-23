0d5488c00e1a70bd00e0d4f9c257f77192815e3c
Yannick Nijs <info.yannicknijs@gmail.com>
2015-12-23 13:19:22 +0100

    auth and guest middleware, sign up and sign in redirect when already logged in

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,16 +12,20 @@ Route::get('/', [
 Route::get('/signup', [
 	'uses' => 'AuthController@getSignup',
 	'as' => 'auth.signup',
+	'middleware' => ['guest'],
 ]);
 Route::post('/signup', [
 	'uses' => 'AuthController@postSignup',
+	'middleware' => ['guest'],
 ]);
 Route::get('/signin', [
 	'uses' => 'AuthController@getSignin',
 	'as' => 'auth.signin',
+	'middleware' => ['guest'],	
 ]);
 Route::post('/signin', [
 	'uses' => 'AuthController@postSignin',
+	'middleware' => ['guest'],
 ]);
 Route::get('/signout', [
 	'uses' => 'AuthController@getSignout',
